06ac5abba391d5f16feec43581c004b5190dcca3
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-05-18 20:47:33 +0530

    Updated the apic operator
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -3,18 +3,18 @@ resource local_file write_outputs {
   filename = "gitops-output.json"
 
   content = jsonencode({
-    name        = module.apic.name
-    branch      = module.apic.branch
-    namespace   = module.apic.namespace
-    server_name = module.apic.server_name
-    layer       = module.apic.layer
-    layer_dir   = module.apic.layer == "infrastructure" ? "1-infrastructure" : (module.apic-operator.layer == "services" ? "2-services" : "3-applications")
-    type        = module.apic.type
+    name        = module.apic-operator.name
+    branch      = module.apic-operator.branch
+    namespace   = module.apic-operator.namespace
+    server_name = module.apic-operator.server_name
+    layer       = module.apic-operator.layer
+    layer_dir   = module.apic-operator.layer == "infrastructure" ? "1-infrastructure" : (module.apic-operator.layer == "services" ? "2-services" : "3-applications")
+    type        = module.apic-operator.type
   })
 }
 
 resource local_file write_bin_dir {
   filename = ".bin_dir"
 
-  content = module.apic.bin_dir
+  content = module.apic-operator.bin_dir
 }
