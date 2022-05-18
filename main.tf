50c4bd04baa6167659fd7a3aeb96f41c50b90af4
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-05-18 20:44:45 +0530

    Updated the apic operator
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 locals {
+  name          = "gitops-cp-apic-operator"
   base_name     = "ibm-apic-operator"
   subscription_name = local.base_name
   bin_dir       = module.setup_clis.bin_dir
@@ -32,7 +33,7 @@ module setup_clis {
   source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
 }
 
-resource null_resource create_subscription_yaml {
+resource null_resource create_yaml {
   provisioner "local-exec" {
     command = "${path.module}/scripts/create-yaml.sh '${local.subscription_name}' '${local.subscription_chart_dir}' '${local.subscription_yaml_dir}' '${local.values_file}'"
 
@@ -42,13 +43,13 @@ resource null_resource create_subscription_yaml {
   }
 }
 
-resource null_resource setup_subscription_gitops {
-  depends_on = [null_resource.create_subscription_yaml]
+resource null_resource setup_gitops {
+  depends_on = [null_resource.create_yaml]
 
   triggers = {
     bin_dir = local.bin_dir
     name = local.subscription_name
-    namespace = var.subscription_namespace
+    namespace = var.namespace
     yaml_dir = local.subscription_yaml_dir
     server_name = var.server_name
     layer = local.layer
