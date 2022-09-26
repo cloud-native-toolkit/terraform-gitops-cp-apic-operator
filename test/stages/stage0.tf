944769b7ba424a7083ee8728c373a33a20cde19e
balasgit <bala@us.ibm.com>
2022-09-26 10:28:41 -0500

    update gitops provider

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -1,18 +1,4 @@
-# terraform {
-# }
 
-# module setup_clis {
-#   source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
-
-#   bin_dir = "${path.cwd}/test_bin_dir"
-#   clis = ["kubectl", "oc", "jq"]
-# }
-
-# resource local_file bin_dir {
-#   filename = "${path.cwd}/.bin_dir"
-
-#   content = module.setup_clis.bin_dir
-# }
 
 terraform {
   required_providers {
